807af7e9f9a541080cac04e9484c91653813d9bc
shaan <46220449+luckypackets@users.noreply.github.com>
2022-06-01 22:46:21 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt install curl wget apt-transport-https net-tools dirmngr htop pydf screen build-essential cmake subversion git automake unrar unzip mediainfo pkg-config libcurl4 libcurl4-openssl-dev libssl-dev libtool libfuse-dev libcppunit-dev libsigc++-2.0-dev libncurses5-dev libxml++2.6-dev firmware-linux firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree dkms linux-headers-$(uname -r)
