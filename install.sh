085abac9b5de9c4e7901172a31d17b2613e30d58
shaan <46220449+luckypackets@users.noreply.github.com>
2022-06-27 21:58:30 +0200

    added how to for updating kernel

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo apt install curl wget apt-transport-https net-tools dirmngr htop pydf screen build-essential cmake subversion git automake unrar unzip mediainfo pkg-config libcurl4 libcurl4-openssl-dev libssl-dev libtool libfuse-dev libcppunit-dev libsigc++-2.0-dev libncurses5-dev libxml++2.6-dev firmware-linux firmware-linux-free firmware-linux-nonfree firmware-misc-nonfree dkms linux-headers-$(uname -r) lm-sensors intel-microcode amd64-microcode tuned menu debian-keyring vim wget gnupg2 build-essential dwarves python3 libncurses-dev flex bison libssl-dev bc libelf-dev
+
+https://techviewleo.com/how-to-install-linux-kernel-on-debian/
